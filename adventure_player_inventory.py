e95332487b11deeaeafb22ece42202ce0a019dea
Josh Guice <jguice@gmail.com>
2018-08-15 17:05:35 -0700

    feat(inventory): move and simplify

--- adventure_player_inventory.py
+++ adventure_player_inventory.py
@@ -1,5 +1,7 @@
+from pprint import pprint
 
-class Inventory: 
+
+class Inventory:
     def __init__(self):
         self.playerInventory = {
             'health' : 50,
@@ -12,6 +14,47 @@ class Inventory:
             'string' : 0,
             'coconut milk' : 0,
         }
+
+    def print_inventory(self):
+        pprint(self.playerInventory)
+        # print('You have:')
+        # if self.playerInventory['sticks'] >= 1:
+        #     print(str(self.playerInventory['sticks']) + ' sticks')
+        # else:
+        #     print('[]')
+        # if self.playerInventory['rocks'] >= 1:
+        #     print(str(self.playerInventory['rocks']) + ' rocks')
+        # else:
+        #     print('[]')
+        # if self.playerInventory['wood'] >= 1:
+        #     print(str(self.playerInventory['wood']) + ' wood')
+        # else:
+        #     print('[]')
+        # if self.playerInventory['stone'] >= 1:
+        #     print(str(self.playerInventory['stone']) + ' stone')
+        # else:
+        #     print('[]')
+        # if self.playerInventory['leaves'] >= 1:
+        #     print(str(self.playerInventory['leaves']) + ' leaves')
+        # else:
+        #     print('[]')
+        # if self.playerInventory['coconuts'] >= 1:
+        #     print(str(self.playerInventory['coconuts']) + ' coconuts')
+        # else:
+        #     print('[]')
+        # if self.playerInventory['string'] >= 1:
+        #     print(str(self.playerInventory['string']) + ' string')
+        # else:
+        #     print('[]')
+        # if self.playerInventory['coconut milk'] >= 1:
+        #     print(str(self.playerInventory['coconut milk']))
+        # else:
+        #     print([])
+        # if self.playerInventory['health'] >= 1:
+        #     print(str(self.playerInventory['health']) + ' health')
+        # else:
+        #     print('You\'ve broken the game. Good job.')
+
     
 
 
