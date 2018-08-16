43a79fa8018b5f299d1a17af868664f2a3c41efb
Skye Guice <skye@guices.net>
2018-08-16 12:51:28 -0700

    fixed ac

--- activities.py
+++ activities.py
@@ -10,6 +10,12 @@ class Activities:
     regionsSafe = ['centerb2', 'mineb2II']
     regionsBeachy = ['beachb3', 'beachc1', 'beachc2', 'beachc3']
 
+    ###
+
+    inventoryAfterHouse = 'Unchecked'
+
+    ###
+
     def basic_crafting(self, inventory, conv_factor, item_to_craft):
 
         num_to_craft = int(input('How many? ').lower().strip())
@@ -22,17 +28,19 @@ class Activities:
         else:
             print('Not enough resources!')
 
-    def advanced_crafting(self, craftingA, craftingB, craftingC, craftingD, craftingE, amountA, amountB, amountC,
-                          amountD, amountE, result, inventory):
-        if inventory.playerInventory[craftingA] >= amountA and inventory.playerInventory[craftingB] >= amountB and \
-                inventory.playerInventory[craftingC] >= amountC and inventory.playerInventory[craftingD] >= amountD and \
-                inventory.playerInventory[craftingE] >= amountE:
-            inventory.playerInventory[craftingA] -= amountA
-            inventory.playerInventory[craftingB] -= amountB
-            inventory.playerInventory[craftingC] -= amountC
-            inventory.playerInventory[craftingD] -= amountD
-            inventory.playerInventory[craftingE] -= amountE
-            inventory.playerInventory[result] = True
+    def advanced_crafting(self, raw_materialA, raw_materialB, raw_materialC, raw_materialD, raw_materialE, costA, costB, costC,
+                          costD, costE, item_to_craft, inventory):
+        if inventory.playerInventory[raw_materialA] >= costA and \
+                inventory.playerInventory[raw_materialB] >= costB and \
+                inventory.playerInventory[raw_materialC] >= costC and \
+                inventory.playerInventory[raw_materialD] >= costD and \
+                inventory.playerInventory[raw_materialE] >= costE:
+            inventory.playerInventory[raw_materialA] -= costA
+            inventory.playerInventory[raw_materialB] -= costB
+            inventory.playerInventory[raw_materialC] -= costC
+            inventory.playerInventory[raw_materialD] -= costD
+            inventory.playerInventory[raw_materialE] -= costE
+            inventory.playerInventory[item_to_craft] = True
         else:
             print('Not enough resources')
 
@@ -59,34 +67,43 @@ class Activities:
                 else:
                     print('You have no coconuts!')
             elif self.command in ['craft', 'rest']:
+
+                #WHY ISNT THIS WORKING?!?!?!?!?
+
+                global inventoryAfterHouse
+                inventoryAfterHouse = 'checked'
+
+                ###
+
                 if location.currentRegion['id'] in Activities.regionsSafe:
                     if self.command == 'rest':
                         if inventory.playerInventory['health'] < 50:
                             if inventory.playerInventory['house'] == False:
                                 if int(inventory.playerInventory['health']) < 20:
                                     inventory.playerInventory['health'] = 20
-                                    print('Your health is: ' + str(inventory.playerInventory['health']))
                                 else:
                                     print('Already feeling well!')
                             else:
                                 inventory.playerInventory['health'] = 50
-                                print('Your health is: ' + str(inventory.playerInventory['health']))
                         else:
                             print('Already feeling well!')
                     else:
                         if inventory.playerInventory['house'] == False:
                             print('What would you like to craft?')
-                            print('Stone cost: 5 rock --- crafting material')
-                            print('Wood cost: 5 sticks --- crafting material')
-                            print('String cost: 5 leaves --- crafting material')
+                            print('Stone cost: 2 rock --- crafting material')
+                            print('Wood cost: 2 sticks --- crafting material')
+                            print('String cost: 2 leaves --- crafting material')
                             print('Coconut milk cost: 3 coconuts --- Heals 5 health')
                             print('House cost: 20 wood, 20 stone, 20 string --- lets you rest up to 50 health')
                             self.command = input().lower().strip()
 
                             if self.command in ['stone', 'wood', 'string']:
-                                self.basic_crafting(inventory, 5, self.command)
-                            else:
+                                self.basic_crafting(inventory, 2, self.command)
+                            elif self.command in ['coconut milk']:
                                 self.basic_crafting(inventory, 3, self.command)
+                            else:
+                                self.advanced_crafting('stone', 'wood', 'string', 'rocks', 'sticks',
+                                                       20, 20, 20, 0, 0, 'house', inventory)
                         else:
                             print('What would you like to craft?')
                             print('Stone cost: 5 rock --- crafting material')
@@ -98,19 +115,19 @@ class Activities:
                             if self.command in ['stone', 'wood', 'string', 'coconut milk']:
                                 if self.command in ['stone']:
                                     amount = input('How many? ')
-                                    if inventory.playerInventory['rocks'] >= 5 * int(amount):
+                                    if inventory.playerInventory['rocks'] >= 2 * int(amount):
                                         inventory.playerInventory[str(self.command)] += 1 * int(amount)
-                                        inventory.playerInventory['rocks'] -= 5 * int(amount)
+                                        inventory.playerInventory['rocks'] -= 2 * int(amount)
                                 elif self.command in ['wood']:
                                     amount = input('How many? ')
-                                    if inventory.playerInventory['sticks'] >= 5 * int(amount):
+                                    if inventory.playerInventory['sticks'] >= 2 * int(amount):
                                         inventory.playerInventory[str(self.command)] += 1 * int(amount)
-                                        inventory.playerInventory['sticks'] -= 5 * int(amount)
+                                        inventory.playerInventory['sticks'] -= 2 * int(amount)
                                 elif self.command in ['string']:
                                     amount = input('How many? ')
-                                    if inventory.playerInventory['leaves'] >= 5 * int(amount):
+                                    if inventory.playerInventory['leaves'] >= 2 * int(amount):
                                         inventory.playerInventory[str(self.command)] += 1 * int(amount)
-                                        inventory.playerInventory['leaves'] -= 5 * int(amount)
+                                        inventory.playerInventory['leaves'] -= 2 * int(amount)
                                 elif self.command in ['coconut milk']:
                                     amount = input('How many? ')
                                     if inventory.playerInventory['coconuts'] >= 3 * int(amount):
@@ -130,22 +147,22 @@ class Activities:
                 if location.currentRegion['id'] in Activities.regionsRocky:
                     if self.command in ['pick up rocks']:
                         inventory.playerInventory['rocks'] += 10
-                        inventory.playerInventory['health'] -= 2
+                        inventory.playerInventory['health'] -= 1
                         print('You have ' + str(inventory.playerInventory['rocks']) + ' rocks')
                     else:
                         print(Activities.errorMessage)
                 elif location.currentRegion['id'] in Activities.regionsBeachy:
                     if self.command in ['pick up sticks']:
                         inventory.playerInventory['sticks'] += 10
-                        inventory.playerInventory['health'] -= 2
+                        inventory.playerInventory['health'] -= 1
                         print('You have ' + str(inventory.playerInventory['sticks']) + ' sticks')
                     elif self.command in ['pick up leaves']:
                         inventory.playerInventory['leaves'] += 10
-                        inventory.playerInventory['health'] -= 2
+                        inventory.playerInventory['health'] -= 1
                         print('You have ' + str(inventory.playerInventory['leaves']) + ' leaves')
                     elif self.command in ['pick up coconuts']:
                         inventory.playerInventory['coconuts'] += 5
-                        inventory.playerInventory['health'] -= 2
+                        inventory.playerInventory['health'] -= 1
                         print('You have ' + str(inventory.playerInventory['coconuts']) + ' coconuts')
                     else:
                         print(Activities.errorMessage)
--- adventure.py
+++ adventure.py
@@ -1,4 +1,3 @@
-
 from pprint import pprint
 from location import Location
 from activities import Activities
@@ -13,26 +12,35 @@ inventory = Inventory()
 crater = True
 
 while True:
-	
-	print('You are at {}.'.format(location.currentRegion['name']))
-	print(location.currentRegion['text'])
-	
-	if inventory.playerInventory['health'] <= 0:
-		print('You died!')
-		break
-	else:
-		print('Your health is ' + str(inventory.playerInventory['health']))
-	
-	if inventory.playerInventory['house'] == True:
-		print('New crafting recipes learned!')
-	
-	if crater == True:
-		gucci = 'yeet'
-	else:
-		if location.currentRegion == location.regions['cratera3']:
-			print('The ground begins to crumble underneath you!')
-			print('You fall and black out!')
-			inventory.playerInventory['health'] /= 2
-			location.currentRegion = location.regions['cavea3II']
 
-	activities.process_activity_statement(location, inventory)
+    print('You are at {}.'.format(location.currentRegion['name']))
+    print(location.currentRegion['text'])
+
+    if inventory.playerInventory['health'] <= 0:
+        print('You died!')
+        break
+    else:
+        print('Your health is ' + str(inventory.playerInventory['health']))
+
+    ###
+
+    if inventory.playerInventory['house'] == True:
+        if activities.inventoryAfterHouse == 'Unchecked':
+            print('New crafting recipes learned!')
+
+    ###
+
+        else:
+            gucci = 'yeet'
+    else:
+        gucci = 'yeet'
+    if crater == True:
+        gucci = 'yeet'
+    else:
+        if location.currentRegion == location.regions['cratera3']:
+            print('The ground begins to crumble underneath you!')
+            print('You fall and black out!')
+            inventory.playerInventory['health'] /= 2
+            location.currentRegion = location.regions['cavea3II']
+
+    activities.process_activity_statement(location, inventory)
